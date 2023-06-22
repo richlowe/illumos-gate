d469f592f13e318a7d117961520fa6c298861110
mintyray <mintal@alert.sk>
2023-06-23 00:35:31 +0200

    boot_args passed for virt

--- usr/src/psm/stand/boot/aarch64/rpi4/machdep.c
+++ usr/src/psm/stand/boot/aarch64/rpi4/machdep.c
@@ -94,20 +94,21 @@ setup_aux(void)
 }
 
 /*
-	Remove -D and the path from the boot args, look for the next "-" char
-*/
+ * Remove -D and the path from the boot args, look for the next "-" char
+ */
 void
-fix_boot_args(const char *str)
+fix_boot_args(char *str)
 {
         char *s2;
         char *s3;
         int i;
+
         s2 = strstr(str, "-D");
-        if(s2 != NULL){
-                s3 = s2+2;
-                for(i=0; s3[i]!=0 && s3[i]!='-'; i++);
+        if (s2 != NULL) {
+                s3 = s2 + 2;
+                for (i=0; s3[i] != 0 && s3[i] != '-'; i++);
                 s3 = s3 + i;
-                memmove(s2, s3, strlen(s3)+1);
+                memmove(s2, s3, strlen(s3) + 1);
 
         }
 }
@@ -156,10 +157,9 @@ void exitto(int (*entrypoint)())
 		prom_setprop(prom_chosennode(), "bootp-response", pktbuf, blen);
 	} else {
 	}
-	str=prom_bootargs(); //pass the bootargs from u-boot to solaris
-	fix_boot_args(str);
+	str=prom_bootargs(); //pass the bootargs from u-boot to illumos
+	fix_boot_args((char *) str);
 	prom_setprop(prom_chosennode(), "boot-args", (caddr_t)str, strlen(str) + 1);
-	//str = "";
 	prom_setprop(prom_chosennode(), "bootargs", (caddr_t)str, strlen(str) + 1);
 	str = filename;
 	prom_setprop(prom_chosennode(), "whoami", (caddr_t)str, strlen(str) + 1);
