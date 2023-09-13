cf7be0e2c47a3c568db47273708ff0220817a16e
Richard Lowe <richlowe@richlowe.net>
2023-09-13 20:12:46 +0000

    libld: use C99 initialization for target descriptors

--- usr/src/cmd/sgs/libld/common/machrel.sparc.c
+++ usr/src/cmd/sgs/libld/common/machrel.sparc.c
@@ -2151,98 +2151,97 @@ const Target *
 ld_targ_init_sparc(void)
 {
 	static const Target _ld_targ = {
-		{			/* Target_mach */
-			M_MACH,			/* m_mach */
-			M_MACHPLUS,		/* m_machplus */
-			M_FLAGSPLUS,		/* m_flagsplus */
-			M_CLASS,		/* m_class */
-			M_DATA,			/* m_data */
+		.t_m = {
+			.m_mach = M_MACH,
+			.m_machplus = M_MACHPLUS,
+			.m_flagsplus = M_FLAGSPLUS,
+			.m_class = M_CLASS,
+			.m_data = M_DATA,
 
-			M_SEGM_ALIGN,		/* m_segm_align */
-			M_SEGM_ORIGIN,		/* m_segm_origin */
-			M_SEGM_AORIGIN,		/* m_segm_aorigin */
-			M_DATASEG_PERM,		/* m_dataseg_perm */
-			M_STACK_PERM,		/* m_stack_perm */
-			M_WORD_ALIGN,		/* m_word_align */
-						/* m_def_interp */
+			.m_segm_align = M_SEGM_ALIGN,
+			.m_segm_origin = M_SEGM_ORIGIN,
+			.m_segm_aorigin = M_SEGM_AORIGIN,
+			.m_dataseg_perm = M_DATASEG_PERM,
+			.m_stack_perm = M_STACK_PERM,
+			.m_word_align = M_WORD_ALIGN,
 #if	defined(_ELF64)
-			MSG_ORIG(MSG_PTH_RTLD_SPARCV9),
+			.m_def_interp = MSG_ORIG(MSG_PTH_RTLD_SPARCV9),
 #else
-			MSG_ORIG(MSG_PTH_RTLD),
+			.m_def_interp = MSG_ORIG(MSG_PTH_RTLD),
 #endif
 
 			/* Relocation type codes */
-			M_R_ARRAYADDR,		/* m_r_arrayaddr */
-			M_R_COPY,		/* m_r_copy */
-			M_R_GLOB_DAT,		/* m_r_glob_dat */
-			M_R_JMP_SLOT,		/* m_r_jmp_slot */
-			M_R_NUM,		/* m_r_num */
-			M_R_NONE,		/* m_r_none */
-			M_R_RELATIVE,		/* m_r_relative */
-			M_R_REGISTER,		/* m_r_register */
+			.m_r_arrayaddr = M_R_ARRAYADDR,
+			.m_r_copy = M_R_COPY,
+			.m_r_glob_dat = M_R_GLOB_DAT,
+			.m_r_jmp_slot = M_R_JMP_SLOT,
+			.m_r_num = M_R_NUM,
+			.m_r_none = M_R_NONE,
+			.m_r_relative = M_R_RELATIVE,
+			.m_r_register = M_R_REGISTER,
 
 			/* Relocation related constants */
-			M_REL_DT_COUNT,		/* m_rel_dt_count */
-			M_REL_DT_ENT,		/* m_rel_dt_ent */
-			M_REL_DT_SIZE,		/* m_rel_dt_size */
-			M_REL_DT_TYPE,		/* m_rel_dt_type */
-			M_REL_SHT_TYPE,		/* m_rel_sht_type */
+			.m_rel_dt_count = M_REL_DT_COUNT,
+			.m_rel_dt_ent = M_REL_DT_ENT,
+			.m_rel_dt_size = M_REL_DT_SIZE,
+			.m_rel_dt_type = M_REL_DT_TYPE,
+			.m_rel_sht_type = M_REL_SHT_TYPE,
 
 			/* GOT related constants */
-			M_GOT_ENTSIZE,		/* m_got_entsize */
-			M_GOT_XNumber,		/* m_got_xnumber */
+			.m_got_entsize = M_GOT_ENTSIZE,
+			.m_got_xnumber = M_GOT_XNumber,
 
 			/* PLT related constants */
-			M_PLT_ALIGN,		/* m_plt_align */
-			M_PLT_ENTSIZE,		/* m_plt_entsize */
-			M_PLT_RESERVSZ,		/* m_plt_reservsz */
-			M_PLT_SHF_FLAGS,	/* m_plt_shf_flags */
+			.m_plt_align = M_PLT_ALIGN,
+			.m_plt_entsize = M_PLT_ENTSIZE,
+			.m_plt_reservsz = M_PLT_RESERVSZ,
+			.m_plt_shf_flags = M_PLT_SHF_FLAGS,
 
 			/* Section type of .eh_frame/.eh_frame_hdr sections */
-			SHT_PROGBITS,		/* m_sht_unwind */
+			.m_sht_unwind = SHT_PROGBITS,
 
-			M_DT_REGISTER,		/* m_dt_register */
+			.m_dt_register = M_DT_REGISTER,
 		},
-		{			/* Target_machid */
-			M_ID_ARRAY,		/* id_array */
-			M_ID_BSS,		/* id_bss */
-			M_ID_CAP,		/* id_cap */
-			M_ID_CAPINFO,		/* id_capinfo */
-			M_ID_CAPCHAIN,		/* id_capchain */
-			M_ID_DATA,		/* id_data */
-			M_ID_DYNAMIC,		/* id_dynamic */
-			M_ID_DYNSORT,		/* id_dynsort */
-			M_ID_DYNSTR,		/* id_dynstr */
-			M_ID_DYNSYM,		/* id_dynsym */
-			M_ID_DYNSYM_NDX,	/* id_dynsym_ndx */
-			M_ID_GOT,		/* id_got */
-			M_ID_GOTDATA,		/* id_gotdata */
-			M_ID_HASH,		/* id_hash */
-			M_ID_INTERP,		/* id_interp */
-			M_ID_UNKNOWN,		/* id_lbss (unused) */
-			M_ID_LDYNSYM,		/* id_ldynsym */
-			M_ID_NOTE,		/* id_note */
-			M_ID_NULL,		/* id_null */
-			M_ID_PLT,		/* id_plt */
-			M_ID_REL,		/* id_rel */
-			M_ID_STRTAB,		/* id_strtab */
-			M_ID_SYMINFO,		/* id_syminfo */
-			M_ID_SYMTAB,		/* id_symtab */
-			M_ID_SYMTAB_NDX,	/* id_symtab_ndx */
-			M_ID_TEXT,		/* id_text */
-			M_ID_TLS,		/* id_tls */
-			M_ID_TLSBSS,		/* id_tlsbss */
-			M_ID_UNKNOWN,		/* id_unknown */
-			M_ID_UNWIND,		/* id_unwind */
-			M_ID_UNWINDHDR,		/* id_unwindhdr */
-			M_ID_USER,		/* id_user */
-			M_ID_VERSION,		/* id_version */
+		.t_id = {
+			.id_array = M_ID_ARRAY,
+			.id_bss = M_ID_BSS,
+			.id_cap = M_ID_CAP,
+			.id_capinfo = M_ID_CAPINFO,
+			.id_capchain = M_ID_CAPCHAIN,
+			.id_data = M_ID_DATA,
+			.id_dynamic = M_ID_DYNAMIC,
+			.id_dynsort = M_ID_DYNSORT,
+			.id_dynstr = M_ID_DYNSTR,
+			.id_dynsym = M_ID_DYNSYM,
+			.id_dynsym_ndx = M_ID_DYNSYM_NDX,
+			.id_got = M_ID_GOT,
+			.id_gotdata = M_ID_GOTDATA,
+			.id_hash = M_ID_HASH,
+			.id_interp = M_ID_INTERP,
+			.id_lbss = M_ID_UNKNOWN, /* unused */
+			.id_ldynsym = M_ID_LDYNSYM,
+			.id_note = M_ID_NOTE,
+			.id_null = M_ID_NULL,
+			.id_plt = M_ID_PLT,
+			.id_rel = M_ID_REL,
+			.id_strtab = M_ID_STRTAB,
+			.id_syminfo = M_ID_SYMINFO,
+			.id_symtab = M_ID_SYMTAB,
+			.id_symtab_ndx = M_ID_SYMTAB_NDX,
+			.id_text = M_ID_TEXT,
+			.id_tls = M_ID_TLS,
+			.id_tlsbss = M_ID_TLSBSS,
+			.id_unknown = M_ID_UNKNOWN,
+			.id_unwind = M_ID_UNWIND,
+			.id_unwindhdr = M_ID_UNWINDHDR,
+			.id_user = M_ID_USER,
+			.id_version = M_ID_VERSION,
 		},
-		{			/* Target_nullfunc */
-			nullfunc_tmpl,		/* nf_template */
-			sizeof (nullfunc_tmpl),	/* nf_size */
+		.t_nf = {
+			.nf_template = nullfunc_tmpl,
+			.nf_size = sizeof (nullfunc_tmpl),
 		},
-		{			/* Target_fillfunc */
+		.t_ff = {
 			/*
 			 * On sparc, special filling of executable sections
 			 * is undesirable, and the default 0 fill supplied
@@ -2266,37 +2265,36 @@ ld_targ_init_sparc(void)
 			 *	of this case and explicitly supply NOP fill.
 			 *	Hence, there is no role for the link-editor.
 			 */
-			NULL			/* ff_execfill */
+			.ff_execfill = NULL,
 		},
-		{			/* Target_machrel */
-			reloc_table,
-
-			ld_init_rel,		/* mr_init_rel */
-			ld_mach_eflags,		/* mr_mach_eflags */
-			ld_mach_make_dynamic,	/* mr_mach_make_dynamic */
-			ld_mach_update_odynamic, /* mr_mach_update_odynamic */
-			ld_calc_plt_addr,	/* mr_calc_plt_addr */
-			ld_perform_outreloc,	/* mr_perform_outreloc */
-			ld_do_activerelocs,	/* mr_do_activerelocs */
-			ld_add_outrel,		/* mr_add_outrel */
-			ld_reloc_register,	/* mr_reloc_register */
-			ld_reloc_local,		/* mr_reloc_local */
-			ld_reloc_GOTOP,		/* mr_reloc_GOTOP */
-			ld_reloc_TLS,		/* mr_reloc_TLS */
-			ld_assign_got,		/* mr_assign_got */
-			ld_find_got_ndx,	/* mr_find_got_ndx */
-			ld_calc_got_offset,	/* mr_calc_got_offset */
-			ld_assign_got_ndx,	/* mr_assign_got_ndx */
-			ld_assign_plt_ndx,	/* mr_assign_plt_ndx */
-			ld_allocate_got,	/* mr_allocate_got */
-			ld_fillin_gotplt,	/* mr_fillin_gotplt */
+		.t_mr = {
+			.mr_reloc_table = reloc_table,
+			.mr_init_rel = ld_init_rel,
+			.mr_mach_eflags = ld_mach_eflags,
+			.mr_mach_make_dynamic = ld_mach_make_dynamic,
+			.mr_mach_update_odynamic = ld_mach_update_odynamic,
+			.mr_calc_plt_addr = ld_calc_plt_addr,
+			.mr_perform_outreloc = ld_perform_outreloc,
+			.mr_do_activerelocs = ld_do_activerelocs,
+			.mr_add_outrel = ld_add_outrel,
+			.mr_reloc_register = ld_reloc_register,
+			.mr_reloc_local = ld_reloc_local,
+			.mr_reloc_GOTOP = ld_reloc_GOTOP,
+			.mr_reloc_TLS = ld_reloc_TLS,
+			.mr_assign_got = ld_assign_got,
+			.mr_find_got_ndx = ld_find_got_ndx,
+			.mr_calc_got_offset = ld_calc_got_offset,
+			.mr_assign_got_ndx = ld_assign_got_ndx,
+			.mr_assign_plt_ndx = ld_assign_plt_ndx,
+			.mr_allocate_got = ld_allocate_got,
+			.mr_fillin_gotplt = ld_fillin_gotplt,
 		},
-		{			/* Target_machsym */
-			ld_reg_check_sparc,	/* ms_reg_check */
-			ld_mach_sym_typecheck_sparc, /* ms_mach_sym_typecheck */
-			ld_is_regsym_sparc,	/* ms_is_regsym */
-			ld_reg_find_sparc,	/* ms_reg_find */
-			ld_reg_enter_sparc	/* ms_reg_enter */
+		.t_ms = {
+			.ms_reg_check = ld_reg_check_sparc,
+			.ms_mach_sym_typecheck = ld_mach_sym_typecheck_sparc,
+			.ms_is_regsym = ld_is_regsym_sparc,
+			.ms_reg_find = ld_reg_find_sparc,
+			.ms_reg_enter = ld_reg_enter_sparc,
 		}
 	};
 
