2b05de87650b3b8ab47cf942ee36399889f8c646
Richard Lowe <richlowe@richlowe.net>
2023-12-03 00:26:56 +0000

    getzoneid: return `GLOBAL_ZONEID` rather than crashing in early boot
    If `curproc->p_zone` is `NULL` then we're in early boot before zones is initialized,
    and `curproc` is `p0`.  In this case we can only be the global zone.
    
    I've done this so that panics in early boot don't recursively panic attempting
    to show the system dump messages on the user console which calls
    `getzoneid` in `cprintf` when `SL_USER`, this makes life easier for debugging,
    and I don't believe there is another valid case where `p_zone == NULL`, but
    I'm not 100% on the correctness of this.

--- usr/src/uts/common/os/zone.c
+++ usr/src/uts/common/os/zone.c
@@ -3034,7 +3034,15 @@ out:
 zoneid_t
 getzoneid(void)
 {
-	return (curproc->p_zone->zone_id);
+	proc_t *p = curproc;
+
+	/* This may happen for p0 during early boot, but not otherwise */
+	if (p->p_zone == NULL) {
+		ASSERT3P(p, ==, &p0);
+		return (GLOBAL_ZONEID);
+	}
+
+	return (p->p_zone->zone_id);
 }
 
 /*
