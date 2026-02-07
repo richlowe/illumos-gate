9528353088e07cc4672e2613c944f685dfcfc004
Richard Lowe <richlowe@richlowe.net>
2026-02-07 21:35:54 +0000

    armv8/vm: replace `boot_reserve` with something easier to read(?)
    Recurse down the page tables, rather than keeping our own stack and iterating,
    the recursion is bounded by the maximum paging level, we won't blow the stack.
    
    We don't need to care about physical address size, etc, because the
    non-relevant bits are RES0.
    
    We should really be using `boot_mapin` like x86 does, but I don't understand
    all the implications of that, and whether we should use it, or use it as part
    of the implementation of something like this.

--- usr/src/uts/armv8/sys/machsystm.h
+++ usr/src/uts/armv8/sys/machsystm.h
@@ -75,7 +75,7 @@ extern void get_system_configuration(void);
 extern page_t *page_get_physical(uintptr_t);
 extern void *mach_cpucontext_alloc(struct cpu *);
 extern void mmu_init(void);
-extern void boot_reserve(void);
+extern uint64_t reserve_boot_pages(pte_t *, uintptr_t, uint_t);
 extern void kcpc_hw_init(cpu_t *cp);
 extern int mach_cpu_create_device_node(cpu_t *, dev_info_t **);
 extern void send_dirint(int, int);
